ee69e7eafc741f569adad7402b535719392b47d9
tracymuzaki <tracymuzaki12@gmail.com>
2022-07-28 08:35:23 +0300

    trial

---
+++ list.py
@@ -0,0 +1,19 @@
+# #1
+# L = list('abc')
+# print(L)
+
+#1
+# w = {
+#     "country": "uganda",
+#     "people": "ugandan",
+#     "tribe": "alur"
+# }
+# p = w["country"]
+# print(p)
+
+# s = str(33)
+# print(s)
+#  w = ("a,b,c,d,e,f,g,h,i")
+# print(w[5:-1])
+s = "Hello," + "World"
+print(s)
